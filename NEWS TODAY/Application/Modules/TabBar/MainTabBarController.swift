59d4b496cb7436396948aab5cdfa104b195e5c57
Daniil Murzin <danmur92@mail.ru>
2024-10-28 12:12:36 +0500

    add UITabBarAppearance

--- NEWS TODAY/Application/Modules/TabBar/MainTabBarController.swift
+++ NEWS TODAY/Application/Modules/TabBar/MainTabBarController.swift
@@ -9,7 +9,24 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     
-   override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
+        setupTabBar()
+    }
+
+    // MARK: Private methods
+    private func setupTabBar() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.backgroundColor = .white
+        tabBar.tintColor = .purplePrimary
+        tabBar.unselectedItemTintColor = .greyLight
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+
+        tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        tabBar.layer.cornerRadius = 12
+        tabBar.layer.borderColor = UIColor.greyLight.cgColor
+        tabBar.layer.borderWidth = 0.5
+        tabBar.clipsToBounds = true
     }
 }
